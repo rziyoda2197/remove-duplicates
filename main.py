b509c46a59a0e6281678b72e8f6fa724c7c3af8b
rziyoda2197 <rziyoda2197@gmail.com>
2026-04-18 15:39:04 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,50 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def add_node(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+
+    def remove_duplicates(self):
+        seen = set()
+        current = self.head
+        prev = None
+        while current:
+            if current.data in seen:
+                prev.next = current.next
+            else:
+                seen.add(current.data)
+                prev = current
+            current = current.next
+
+    def print_list(self):
+        current = self.head
+        while current:
+            print(current.data, end=" ")
+            current = current.next
+        print()
+
+# Test the code
+linked_list = LinkedList()
+linked_list.add_node(1)
+linked_list.add_node(2)
+linked_list.add_node(2)
+linked_list.add_node(3)
+linked_list.add_node(4)
+linked_list.add_node(4)
+linked_list.add_node(5)
+linked_list.print_list()  # Output: 1 2 2 3 4 4 5
+linked_list.remove_duplicates()
+linked_list.print_list()  # Output: 1 2 3 4 5
